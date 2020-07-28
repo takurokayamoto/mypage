0ffb4f04663494e539daa9fabab1f9c8cc4135a5
Takuro Kayamoto <takurokayamoto@MacBook-Pro.local>
2020-07-28 21:13:26 +0900

    frist commit with react and node.js

---
+++ client/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Edit and save to reload.</p>
+                <a>Learn React</a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
