58908cbe94bb9417a896c8cebb76f9c9c1aeb053
Takuro Kayamoto <takurokayamoto@MacBook-Pro.local>
2020-08-08 20:55:40 +0900

    change port to 5000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
