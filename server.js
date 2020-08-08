c036d57d2c6c817e8a94fdd55f3b12ffa06f0272
Takuro Kayamoto <takurokayamoto@MacBook-Pro.local>
2020-08-08 21:21:35 +0900

    fixed bug

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
@@ -10,10 +11,8 @@ app.use(express.static("public"));
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
-
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
