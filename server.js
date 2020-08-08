64d043c996099701f86c4b203864db1e58894d5d
Takuro Kayamoto <takurokayamoto@MacBook-Pro.local>
2020-08-08 21:20:31 +0900

    fixed bug

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const __dirname = path.resolve();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
 }
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
