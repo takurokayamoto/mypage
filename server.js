37eb7c9680cd979c4a38149e9155b4daefecf3ec
Takuro Kayamoto <takurokayamoto@MacBook-Pro.local>
2020-08-09 09:19:09 +0900

    able to connect React and backendgit add .git add .git add .

--- server.js
+++ server.js
@@ -9,11 +9,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/client/build")));
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, "client/build")));
+} else {
+    app.use(express.static(path.join(__dirname, "client/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    });
 }
 
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+// // Handle React routing, return all requests to React app
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
