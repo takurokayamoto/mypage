a43ca869f121257c2becc9965a2f9d862e60ef14
Takuro Kayamoto <takurokayamoto@MacBook-Pro.local>
2020-08-09 09:37:19 +0900

    fixed gitignore file with build folder

---
+++ client/build/precache-manifest.0d294bf6ca99732f87af9943e2a446ed.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2468159937742b9a8e86f3021d8cc8db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f3f913f4358401d9036",
+    "url": "/static/js/2.582c38d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.582c38d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6215b4fc33ad8cab959",
+    "url": "/static/js/main.b086c2a3.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "91702ae12ad7abc9c64c67e44f17094c",
+    "url": "/static/media/airticket.91702ae1.svg"
+  },
+  {
+    "revision": "6f063afe8c86fc8f7734afab5eeb676c",
+    "url": "/static/media/avatar.6f063afe.png"
+  },
+  {
+    "revision": "5c14977aa53b3e4ff3dcc688b0541237",
+    "url": "/static/media/calclator.5c14977a.svg"
+  },
+  {
+    "revision": "829af7aa7c3ea6eb6607aed24f70b8ad",
+    "url": "/static/media/homeVideo.829af7aa.mp4"
+  },
+  {
+    "revision": "c59c3f1c83d3685777a571e24b807786",
+    "url": "/static/media/mappyMap.c59c3f1c.svg"
+  },
+  {
+    "revision": "3df30a1e2ee3e1eb9cc81e20dc0966c6",
+    "url": "/static/media/mappyTitle.3df30a1e.svg"
+  },
+  {
+    "revision": "0a9c0f439e92a060bde8b0aa5379026f",
+    "url": "/static/media/sunglass.0a9c0f43.svg"
+  },
+  {
+    "revision": "6398d1dd8caaa1cc5337804d999fa8a0",
+    "url": "/static/media/third.6398d1dd.jpg"
+  },
+  {
+    "revision": "5736fd3c295e741da5c687c53b624f70",
+    "url": "/static/media/titleIPhone.5736fd3c.svg"
+  }
+]);
